e204ff642bcc7b60d3d10e50ce7e2f8352899673
lizzaraga <fomekongchristmael@gmail.com>
2023-05-15 21:41:10 +0100

    First commit

---
+++ src/test/java/com/lizzaraga/eventer/EventerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizzaraga.eventer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
